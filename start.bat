1a91b00eb4de24d2857298a52a250e4ebd2817ba
Alexa Softie <softiealexa191@gmail.com>
2025-07-03 09:56:13 +0000

    do i need to always run npm run dev to run this , can u suggest somethin

---
+++ start.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Starting Trackademic...
+echo.
+echo Your app will be available at http://localhost:9002
+echo.
+echo Close this window to stop the app.
+npm run start
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Starting Trackademic..."
+echo ""
+echo "Your app will be available at http://localhost:9002"
+echo ""
+echo "Press Ctrl+C in this window to stop the app."
+npm run start
