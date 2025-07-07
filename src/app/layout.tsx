448a65bba86c2c0fb868fbf1635666124f2adb3c
Alexa Softie <softiealexa191@gmail.com>
2025-07-07 17:37:08 +0000

    npm i @vercel/speed-insights import { SpeedInsights } from "@vercel/spee

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
@@ -97,6 +98,7 @@ export default function RootLayout({
           <Toaster />
         </DataProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
